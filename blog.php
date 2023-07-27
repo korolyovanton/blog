56ce9bf4bca0ed0bb46565e933d25799bd031297
Anton Korolyov <antonkor12@gmail.com>
2023-07-27 17:17:15 +0300

    first commit

---
+++ blog.php
@@ -0,0 +1,40 @@
+<?php
+
+$textStorage = [];
+$elementsCount = count($textStorage);
+
+function add (&$textStorage, string $title, string $text) {
+    $textStorage[] = [
+        'title' => $title,
+        'text' => $text
+       ];
+}
+
+add($textStorage, 'Заголовок 1', 'Текст 1');
+add($textStorage, 'Заголовок 2', 'Текст 2');
+//var_dump($textStorage[0]['title']);
+var_dump($textStorage);
+
+function remove (int $index, &$textStorage) {
+    if (array_key_exists($index, $textStorage)) {
+        unset($textStorage[$index]);
+        return true;
+    }
+    return false;
+}
+
+remove(0, $textStorage, $elementsCount);
+var_dump($textStorage);
+
+function edit (int $index, string $title, string $text, &$textStorage) {
+    if (array_key_exists($index, $textStorage)) {
+        $textStorage[$index] = [
+            'title' => $title,
+            'text' => $text
+        ];
+    }
+    return false;
+}
+
+edit(1, 'Измененный заголовок', 'Измененный текст', $textStorage);
+var_dump($textStorage);
